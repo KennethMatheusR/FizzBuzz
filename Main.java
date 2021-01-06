824cfdf9f2cf5a1c088fd4ea0c50a8041aa80280
KENNETH MATHEUS R <kenneth.matheus@gmail.com>
2021-01-06 02:29:57 +0000

    practice FizzBuzz
    1 base in a count 1 to 100
    print Fizz if the value is multiple by 3, print Buzz if the value is multiple by 5, and print the number if its not the case

--- Main.java
+++ Main.java
@@ -1,11 +1,26 @@
 class Main {
   public static void main(String[] args) {
+        // tittle message
+        System.out.println("FizzBuzz practice");
+
+    //loop to print the values
     for(int count = 1; count <= 100; count++){
-      
-        System.out.println(count);
+        // to get the first condition if the value is mutiple of 3 and 5
+        if(count %3 == 0 && count %5 == 0){
+          System.out.println("FizzBuzz");
+        }else if(count %3 == 0){
+          // to get the second condition if the value is multiple of 3
+          System.out.println("Fizz");
+        }else if(count %5 == 0){
+          //to get the third condition , if value is multiple of 5 
+          System.out.println("Buzz");
+        }else{
+          //if not alll above print the number
+          System.out.println(count);
+        }
+
+
     }
 
-
-    System.out.println("FizzBuzz practice");
   }
 }
